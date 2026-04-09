515f59714866bb734a26345f12e6146a90789be0
darkestfloyd <nischal.bhatewara@gmail.com>
2026-04-09 18:19:06 -0400

    refactor BPE to operate with bytes

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -2,32 +2,47 @@ import numpy as np
 import regex
 from collections import defaultdict
 
-def count_pairs(word_counts):
-    pair_counts = defaultdict(int)
+def count_pairs(word_counts: dict[list[bytes], int]) -> dict[tuple[bytes, bytes], int]:
+    pair_counts: dict[tuple[bytes, bytes], int] = defaultdict(int)
     for word in word_counts:
         for i in range(len(word)-1):
-            pair_key = word[i], word[i+1]
+            pair_key = (word[i], word[i+1])
             pair_counts[pair_key] += 1
-    return(pair_counts)
+    return pair_counts 
 
-def merge_max(word_counts, max_pair, new_idx):
-    new_counts = defaultdict(int)
+def merge_max(word_counts: dict[tuple[bytes, ...], int], max_pair: tuple[bytes, bytes]) -> dict[list[bytes], int]:
+    new_counts: dict[list[bytes], int] = defaultdict(int)
     for word in word_counts:
-        new_word = word
+        occurances = []
         for i in range(len(word)-1):
             if word[i] == max_pair[0] and word[i+1] == max_pair[1]:
-                # pair in word
-                new_word = word[:i] + (new_idx,) + word[i+2:]
-        new_counts[new_word] = word_counts[word]
+                    # pair in word
+                    occurances.append(i)
+        new_word = []
+        for i in range(len(word)):
+            if i in occurances:
+                new_word.append(b''.join(max_pair))
+            elif i-1 in occurances: continue
+            else:
+                new_word.append(word[i])
+        new_counts[tuple(new_word)] = word_counts[word]
     return new_counts
 
+# count words
+def get_word_counts(words: iter) -> dict[tuple[bytes, ...], int]:
+    word_counts: dict[list[bytes], int] = defaultdict(int)
+    for word in words:
+        sub_word = tuple(bytes([x]) for x in word.group().encode())
+        word_counts[sub_word] += 1
+    return word_counts
+        
 class BPETokenizer:
     def __init__(self):
         self.PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
         self.vocab = {x: bytes([x]) for x in range(256)}
         self.merges = []
         pass
-        
+
     def train(self, input_path: str, 
               vocab_size: int, 
               special_tokens: list[str],
@@ -37,34 +52,29 @@ class BPETokenizer:
 
         # expand vocab with special tokens
         for sp in special_tokens:
-            self.vocab[max(self.vocab) + 1] = sp
+            self.vocab[max(self.vocab) + 1] = sp.encode()
 
         # break str into words
         words = regex.finditer(self.PAT, doc)
+        word_counts = get_word_counts(words)
 
-        # count words
-        word_counts = defaultdict(int)
-        for word in words:
-            sub_word = tuple(word.group().encode())
-            word_counts[sub_word] += 1
-
-        while len(self.vocab) <= vocab_size:
+        sorted_counts = lambda x: sorted(x.items(), key=lambda item: item[1])
+        # print(sorted_counts(word_counts))
+        # print("============================")
+        while len(self.vocab) < vocab_size:
             pair_counts = count_pairs(word_counts)
+            # print(sorted_counts(pair_counts))
+            # print("-----------------")
             max_pair = max(pair_counts, key=pair_counts.get)
+            # print(max_pair)
             self.vocab[max(self.vocab) + 1] = max_pair
             self.merges.append(max_pair)
-            word_counts = merge_max(word_counts, max_pair, 
-                                    new_idx=max(self.vocab))
+            word_counts = merge_max(word_counts, max_pair)
             
-        print(self.vocab)
-        print(self.merges)
+        return (self.vocab, self.merges)
 
-        return((self.vocab, self.merges))
 
-# doc = ['low low low low low', 
-#        'lower lower widest widest widest',
-#        'newest newest newest newest newest newest', 
-#        'hello! こんにちは!']
 
 bpe_tokenizer = BPETokenizer()
-print(bpe_tokenizer.train('./tests/fixtures/corpus.en', 1000, ['<|endoftext|>']))
+print(bpe_tokenizer.train('./tests/fixtures/corpus.en', 260, ['<|endoftext|>']))
+# print(bpe_tokenizer.train('./tests/fixtures/test_doc.txt', 260, ['<|endoftext|>']))
